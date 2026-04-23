f6e59937475ebd3400f853508afc072dd2f52148
dasuni <dasunisadeesha12@gmail.com>
2026-04-23 14:34:11 +0530

    create Main.java to start entire api

--- src/main/java/com/smartcampus/Main.java
+++ src/main/java/com/smartcampus/Main.java
@@ -1,17 +1,40 @@
 package com.smartcampus;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import com.smartcampus.config.ApplicationConfig;
+import java.net.URI;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+public class Main {
+
+    // The base URL where API run
+    public static final String BASE_URI = "http://localhost:8080/";
+
+    public static void main(String[] args) throws Exception {
+        //  Create the JAX-RS configuration
+        // ApplicationConfig tells Jersey which classes to scan for @Path annotations
+        ResourceConfig config = new ApplicationConfig();
+
+        //  Create and start the Grizzly HTTP server
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
+                URI.create(BASE_URI),
+                config
+        );
+
+        // Print success message
+        System.out.println("===========================================");
+        System.out.println("  Smart Campus API is running!");
+        System.out.println("  URL: http://localhost:8080/api/v1");
+        System.out.println("  Press ENTER to stop the server...");
+        System.out.println("===========================================");
+
+        // Keep server running until user presses Enter
+        System.in.read();
+
+        // Stop the server when user presses Enter
+        server.stop();
+        System.out.println("Server stopped.");
     }
 }
